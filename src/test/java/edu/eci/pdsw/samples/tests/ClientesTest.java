681359d5e70ccda7339830efa6713767f4939913
Daniel Rodriguez <daniel.rodriguez-tr@mail.escuelaing.edu.co>
2017-03-28 14:33:50 -0500

    correccion prueba cliente ok

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -32,7 +32,7 @@ public class ClientesTest {
     	ServiciosAlquiler sa=ServiciosAlquilerItemsStub.getInstance();
         Cliente cliente=new Cliente("Daniel Rodriguez",101236547, "3186688626", "CALLE 63 #45-86", "daferrotru@hotmail.com");
         sa.registrarCliente(cliente);
-        assertEquals(101236547, sa.consultarClientes().get(sa.consultarClientes().size()-1).getDocumento());
+        assertEquals("Daniel Rodriguez", sa.consultarCliente(101236547).getNombre());
     }   
     
     
